1b39b675cf5e42a94ad27088331c4444dac9414b
sunfeng2016 <sunfeng@nuaa.edu.cn>
2024-02-20 16:22:52 +0800

    add maic controller

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -7,11 +7,15 @@ from .conv_controller import ConvMAC
 from .basic_central_controller import CentralBasicMAC
 from .lica_controller import LICAMAC
 from .dop_controller import DOPMAC
+from .task_controller import TaskMAC
+from .maic_controller import MAICMAC
 
 REGISTRY["basic_mac"] = BasicMAC
 REGISTRY["n_mac"] = NMAC
+REGISTRY["task_mac"] = TaskMAC
 REGISTRY["ppo_mac"] = PPOMAC
 REGISTRY["conv_mac"] = ConvMAC
 REGISTRY["basic_central_mac"] = CentralBasicMAC
 REGISTRY["lica_mac"] = LICAMAC
 REGISTRY["dop_mac"] = DOPMAC
+REGISTRY['maic_mac'] = MAICMAC
